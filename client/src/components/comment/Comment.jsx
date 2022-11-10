69301cc784a7a6ae39c49dbbf49be7074bfdb6d4
Your Name <you@example.com>
2022-11-11 02:14:22 +0530

    Add share functionality

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -3,23 +3,20 @@ import React from "react";
 import "./Comment.css";
 import { useState } from "react";
 import UserModal from "../userModal/UserModal";
-export default Comment = ({ comment, setPost, post }) => {
-
+export default Comment = ({ userID, comment, setPost, post }) => {
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
-  const [userInfo,setUserInfo] = useState(null)
+  const [userInfo, setUserInfo] = useState(null);
   const onUserInfoClick = async (targetUserName) => {
     try {
-      const targetUserInfo = await axios.get(`/users/?username=${targetUserName}`)
-      setIsUserModalOpen(true)
-      setUserInfo(targetUserInfo.data[0])
+      const targetUserInfo = await axios.get(
+        `/users/?username=${targetUserName}`
+      );
+      setIsUserModalOpen(true);
+      setUserInfo(targetUserInfo.data[0]);
+    } catch (err) {
+      toaster.info("Could not fetch the user details");
     }
-    catch (err) {
-      toaster.info("Could not fetch the user details")
-    }
-  }
-
-
-
+  };
 
   const handleDelete = async () => {
     const deletePost = await axios.delete(
@@ -27,31 +24,45 @@ export default Comment = ({ comment, setPost, post }) => {
     );
     setPost(deletePost.data);
   };
+
   return (
     <>
-      <UserModal modalUser={userInfo} isOpen={isUserModalOpen} setIsOpen={setIsUserModalOpen}/>
+      <UserModal
+        modalUser={userInfo}
+        isOpen={isUserModalOpen}
+        setIsOpen={setIsUserModalOpen}
+      />
       <div className="SingleComment">
-      <div className="topSection">
-        <img
-          src={comment.user_id.profilePic ? `http://localhost:5000/images/${comment.user_id.profilePic}` : "http://localhost:5000/images/DAIICT_LOGO.png"}
-          className="profilePic"
-          onClick={() => onUserInfoClick(comment.user_id.username)}
-        />
-        <span className="userName"
-          onClick={() => onUserInfoClick(comment.user_id.username)}
-        >{comment.user_id.username}</span>
-      </div>
-      <div className="bottomSection">
-        <p>{comment.cmt_text}</p>
-        <div>
-          <i
-            onClick={handleDelete}
-            className="fa fa-trash"
-            aria-hidden="true"
-          ></i>
+        <div className="topSection">
+          <img
+            src={
+              comment.user_id.profilePic
+                ? `http://localhost:5000/images/${comment.user_id.profilePic}`
+                : "http://localhost:5000/images/DAIICT_LOGO.png"
+            }
+            className="profilePic"
+            onClick={() => onUserInfoClick(comment.user_id.username)}
+          />
+          <span
+            className="userName"
+            onClick={() => onUserInfoClick(comment.user_id.username)}
+          >
+            {comment.user_id.username}
+          </span>
+        </div>
+        <div className="bottomSection">
+          <p className="commentText">{comment.cmt_text}</p>
+          <div>
+            {comment.user_id._id == userID && (
+              <i
+                onClick={handleDelete}
+                className="fa-regular fa-circle-xmark"
+                aria-hidden="true"
+              ></i>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
